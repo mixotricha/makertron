c170ad5a15219c52953dd2d1630e87d09254cf80
mixotricha <mixotricha@github.org>
2017-08-30 13:18:51 +1000

    Setting deployment to port 80

--- makertron_5.0.1/js/config.js
+++ makertron_5.0.1/js/config.js
@@ -2,7 +2,7 @@
  
 										VERSION        = "5.0.1"
 										CLIENT_ADDRESS = "localhost"  
-										CLIENT_PORT    = "8080"  	
+										CLIENT_PORT    = "80"  	
 										SERVER_ADDRESS = "http://localhost"  
 										SERVER_PATH    = "localhost/"  
 										SERVER_PORT    = "3000"
