eba9bf4030c729da4ab05089d416d9d8c2fb5712
root <connolly.damien@gmail.com>
2017-10-29 02:41:33 +0000

    Updated with version of node-ffi module that supports aarch64.

---
+++ makertron_5.0.2/node-ffi/build/Release/.deps/Release/obj.target/ffi/deps/libffi/src/aarch64/sysv.o.d
@@ -0,0 +1,4 @@
+cmd_Release/obj.target/ffi/deps/libffi/src/aarch64/sysv.o := cc '-DNODE_GYP_MODULE_NAME=ffi' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DPIC' '-DFFI_BUILDING' '-DHAVE_CONFIG_H' '-DNDEBUG' -I/root/.node-gyp/4.8.4/include/node -I/root/.node-gyp/4.8.4/src -I/root/.node-gyp/4.8.4/deps/uv/include -I/root/.node-gyp/4.8.4/deps/v8/include -I../deps/libffi/include -I../deps/libffi/config/linux/arm64  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/ffi/deps/libffi/src/aarch64/sysv.o.d.raw   -c -o Release/obj.target/ffi/deps/libffi/src/aarch64/sysv.o ../deps/libffi/src/aarch64/sysv.S
+Release/obj.target/ffi/deps/libffi/src/aarch64/sysv.o: \
+ ../deps/libffi/src/aarch64/sysv.S
+../deps/libffi/src/aarch64/sysv.S:
---
+++ makertron_5.0.2/node-ffi/build/Release/.deps/Release/obj.target/ffi/deps/libffi/src/raw_api.o.d
@@ -0,0 +1,5 @@
+cmd_Release/obj.target/ffi/deps/libffi/src/raw_api.o := cc '-DNODE_GYP_MODULE_NAME=ffi' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DPIC' '-DFFI_BUILDING' '-DHAVE_CONFIG_H' '-DNDEBUG' -I/root/.node-gyp/4.8.4/include/node -I/root/.node-gyp/4.8.4/src -I/root/.node-gyp/4.8.4/deps/uv/include -I/root/.node-gyp/4.8.4/deps/v8/include -I../deps/libffi/include -I../deps/libffi/config/linux/arm64  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/ffi/deps/libffi/src/raw_api.o.d.raw   -c -o Release/obj.target/ffi/deps/libffi/src/raw_api.o ../deps/libffi/src/raw_api.c
+Release/obj.target/ffi/deps/libffi/src/raw_api.o: \
+ ../deps/libffi/src/raw_api.c ../deps/libffi/include/ffi_common.h
+../deps/libffi/src/raw_api.c:
+../deps/libffi/include/ffi_common.h:
