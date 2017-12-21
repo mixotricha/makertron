2b7bf80be907989de7c7508e338cab4c2bdef5c3
mixotricha <mixotricha@github.org>
2017-12-22 07:54:32 +1100

    Updating the whole source tree to catch some out of main branch changes.

--- makertron_5.0.2/snoodle/build/Release/.deps/Release/obj.target/ffi/deps/libffi/src/x86/unix64.o.d
+++
@@ -1,5 +0,0 @@
-cmd_Release/obj.target/ffi/deps/libffi/src/x86/unix64.o := cc '-DNODE_GYP_MODULE_NAME=ffi' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DPIC' '-DFFI_BUILDING' '-DHAVE_CONFIG_H' '-DNDEBUG' -I/usr/include/nodejs/include/node -I/usr/include/nodejs/src -I/usr/include/nodejs/deps/uv/include -I/usr/include/nodejs/deps/v8/include -I../deps/libffi/include -I../deps/libffi/config/linux/x64  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/ffi/deps/libffi/src/x86/unix64.o.d.raw  -c -o Release/obj.target/ffi/deps/libffi/src/x86/unix64.o ../deps/libffi/src/x86/unix64.S
-Release/obj.target/ffi/deps/libffi/src/x86/unix64.o: \
- ../deps/libffi/src/x86/unix64.S ../deps/libffi/include/fficonfig.h
-../deps/libffi/src/x86/unix64.S:
-../deps/libffi/include/fficonfig.h:
