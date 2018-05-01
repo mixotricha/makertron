4719563d1ab5265910ac9f2c03c83ff0148e1ce3
mixotricha <mixotricha@github.org>
2018-05-01 22:18:32 +1000

    New parser now handles parameters correctly.

--- makertron_5.0.3/js/test.js
+++ makertron_5.0.3/js/test.js
@@ -71,7 +71,16 @@
 		return exclude.reduce( (stack,op) => op === tkn ? stack = true : stack , false )  
 	}
 
-	
+	// =========================================================================================
+	// Is a token a string 
+	// =========================================================================================
+	let isString = token => {	
+		token = token.replace(/([A-Z-a-z])/g,'')
+		token = token.replace(/([0-9])/g,'')
+		token = token.replace(/(['_'])/g,'')
+		return token.length === 0 ? true : false
+	}
+
 	// =========================================================================================
 	// Is this token closure / compound related
 	// =========================================================================================
@@ -195,7 +204,7 @@
 	// ======================================================
 	// convert module declarations to js functions 
 	// ======================================================
-	let reFormatModules = stream => { 
+	let reformatModules = stream => { 
 		let result = [] 
 		// -----------------------------------------------------
 		// Iterate through modules reformating to js syntax
@@ -216,71 +225,66 @@
 		return result 
 	} 
 
-		// ---------------------------------------------------------------
-		// Take arguments to function and split up in to array
-		// More work required here for things not passed as a = b expressions
-		// such as foo(true,false,blah())   
-		// (a,a=b,a=foo([1,2,3]),false,20)
-		// (j=k,m=12,size=[1,2,3],m=sin(20+6))
-		// ([x,0,0,])
-		// (size=5)
-		// ([5,0,0])
-		// ---------------------------------------------------------------
-		let splitArguments = (stream,start,end) => {
+// ===================================================================
+	// Take arguments to function and split up in to array
+	// More work required here for things not passed as a = b expressions
+	// such as foo(true,false,blah())   
+	// (a,a=b,a=foo([1,2,3]),false,20)
+	// (j=k,m=12,size=[1,2,3],m=sin(20+6))
+	// ([x,0,0,])
+	// (size=5)
+	// ([5,0,0])
+	// ==================================================================
+	let splitArguments = (stream,start,end) => {
 
-			let parameters = stream.slice( start , end ) 
-			let consumeDepth = ( lft , rht , index , e ) => { 
-				let tkn = parameters[index] 
-				if ( tkn === lft ) { e = findPair(lft,rht,0,0,index,parameters,tkn=>tkn) }
-				( index <= e && e > 0 ) ? parameters[index] = "_"+parameters[index] : e = 0  
-				if ( index < parameters.length ) consumeDepth( lft , rht , index + 1 , e ) 
-			}
-			consumeDepth( '(' , ')' , 0 , 0 )
-			consumeDepth( '[' , ']' , 0 , 0 ) 
-			consumeDepth( '{' , '}' , 0 , 0 ) 
-			parameters = parameters.map( ( tkn , index ) => tkn === '=' ? ':' : tkn ) 
- 
-			console.log( parameters ) 
-			
-			//return e
+		let parameters = stream.slice( start , end ) 
 
+		 if ( parameters.length === 0 ) return [{}] 
 
-			/*let splits=[], sliced = [] 
-			//splits.push( start + 1 ) 
-			let iterate = ( index , end , token_depth ) => {  
-  			if ( (token_depth === 0 && stream[index] === ",")  ) {  
-					if ( stream[index+2] === "=" ) splits.push(index+2) // pull only first level seperation ',' 
-				}
-    		if ( stream[index] === "[" || stream[index] === "(" ) token_depth++
-    		if ( stream[index] === "]" || stream[index] === ")" ) token_depth--
-				if ( index < end ) iterate(index+1,end,token_depth) 
-  		}
-			iterate(start,end,0) 
-			splits.forEach( index => {	stream[index]=":"	})
-			return stream.slice(start,end)*/   
+		// ------------------------------------------------------------------------------------
+		// blank out all tkns in closure at any depth leaving top level expression relationship behind  
+		// ------------------------------------------------------------------------------------
+		let consumeDepth = ( lft , rht , index , e ) => { 
+			let tkn = parameters[index] 
+			if ( tkn === lft ) { e = findPair(lft,rht,0,0,index,parameters,tkn=>tkn) }
+			( index <= e && e > 0 ) ? parameters[index] = "|||||"+parameters[index] : e = 0  
+			if ( index < parameters.length ) consumeDepth( lft , rht , index + 1 , e ) 
 		}
+		consumeDepth( '(' , ')' , 0 , 0 ) // blank (***)
+		consumeDepth( '[' , ']' , 0 , 0 ) // blank [***]
+		consumeDepth( '{' , '}' , 0 , 0 ) // blank {***}
+		parameters = [","].concat( parameters ) // since all valid expressions begin with comma add one for first expression 
+		let table = parameters.reduce( ( stack , tkn , index ) => tkn === ',' ? stack.concat(index+1) : stack , [] ) // build index of tkn aft ,   
+		// Is next token followed by relation or is it something else. If something else becomes argument. Otherwise key : value pair 
+		table.forEach( (offset,index) => { 
+			if ( isString(parameters[offset]) && parameters[offset+1] === "=" ) {  
+				parameters[offset+1] = ":"
+			}
+			else {
+				parameters.splice( offset , 0 ,  "arg" , ":" ) 
+			}   		  
+		})	
+		return ['{'].concat(parameters.map( tkn => tkn.replace("|||||",'')).slice(1,parameters.length).concat(['}']))  
+	}
 
 	// ==================================================
 	// Reformat calls to functions to be of a jsn format 
 	// ==================================================
 	let reformatFunCalls = stream => { 
-
 		// ----------------------------------------------------------------
 		// iterate through function calls 
 		// ----------------------------------------------------------------
 		let result = [] 
 		let iterate = (index) => { 
 			let tkn = stream[index] 
-
 			if ( isToken(tkn) && !isExclude(tkn) && stream[index+1] === "(" ) {
 				let end = findPair('(',')',0,0,index+1,stream,tkn=>tkn)
 				if ( end !== -1 ) { 
-					//result.push(  splitArguments( stream , index+2, end ) )
 					result.push( tkn , '(' , splitArguments( stream , index+2, end ) )  
+					//console.log( splitArguments( stream , index+2, end ) ) 
 					index = end   	
 				}
 			}
-
 			result.push( stream[index] )   
 			if ( index < stream.length-1 ) iterate(index+1) 
 		}
@@ -329,7 +333,7 @@
 		let iterate = (index) => { 
 			let tkn = set[index]
 			let id = makeId()
-			if ( isToken(tkn) && tkn !== "for" ) {  
+			if ( isToken(tkn) && !isExclude(tkn) ) {  
 				stack.push( { index : index , id : id , token : tkn , parent : parent } ) 
 				let s = findPair('(',')',0,0,index+1,set,tkn=>tkn)+1
 				if ( set[s] === "{" ) {
@@ -344,7 +348,7 @@
 		return lodash.flatten(stack) 
 	} 
 
-	let scad = 'module foo ( s = 6 , j = 9 ) { union ( g = 3 , a = 4 ) { cube (j=k,m=12,size=[1,2,3],m=sin(20+6)); } difference() { for (x=[0:10:20]) rotate ([x,0,0,]) { translate([5,0,0]) { cylinder (size=5); } translate ([90,0,0]) rotate ([5,5,5]) sphere (r=5); } } }'
+	let scad = 'module foo ( s = 6 , j = 9 ) { union ( g = 3 , a = 4 ) { cube (j=k,m=12,size=[1,2,3],sin(20+6)); } difference() { for (x=[0:10:20]) rotate ([x,0,0,]) { translate([5,0,0]) { cylinder (size=5); } translate ([90,0,0]) rotate ([5,5,5]) sphere (r=5); } } }'
 
 	//let scad = fs.readFileSync('test.scad', 'utf8');
 
@@ -360,23 +364,49 @@
   
 	let exclude = [ "for","if" ] 
  	
-	let res = reformatLoops( reFormatModules( healClosure( src ) ) ) 
+	//let res = reformatLoops( reFormatModules( healClosure( src ) ) ) 
 
-	//let res = reformatFunCalls( reFormatModules( healClosure( src ) ) ) 
+	let res = reformatFunCalls( reformatLoops( reformatModules( healClosure( src ) ) ) )  
 
+	let m = buildTree(res,0,res.length-1,makeId(),'root') 
+
+	let poop = res.map( (tkn,index) => { 	
+		let row = m.reduce( (result,tkn) => tkn.index===index ? tkn : result , false)  
+		return row !== false ? tkn+"("+row.id+","+row.parent+")" : tkn 
+ 	}) 
+
+	console.log( streamToString(lodash.flatten(poop)) )
+	
+		union (3IgZo,82IXb) ( { g : 3 , a : 4 } )  
+			cube(QrF5T,3IgZo) ( { j : k , m : 12 , size : [ 1 , 2 , 3 ] , arg : sin ( 20 + 6 ) } ) ; 
+		end() 
+		difference(zwzLm,82IXb) ( [] )  
+			for ( x = 0 ; x <= 20 ; x += 10 ) { 
+				rotate(f6TTt,zwzLm) ( { arg : [ x , 0 , 0 , ] } )  
+					translate(BBL7U,f6TTt) ( { arg : [ 5 , 0 , 0 ] } )  
+						cylinder(R0bOX,BBL7U) ( { size : 5 } ) ; 
+					end() 
+				end() 
+				translate(3NtL5,zwzLm) ( { arg : [ 90 , 0 , 0 ] } )  
+					rotate(jX5LI,3NtL5) ( { arg : [ 5 , 5 , 5 ] } )  
+						sphere(5DnoS,jX5LI) ( { r : 5 } ) ; 
+					end() 
+				end() 
+			} 
+		end() 
+	 
+
+	//console.log( lodash.flatten(res) ) 
  
 	//console.log( res.slice(28,54) ) 
 	
-	console.log( splitArguments(res,28,54) )
+	//console.log( splitArguments(res,28,52) )
  
 	//console.log( res.slice(17,24) ) 
 	//console.log( splitArguments(res,17,24) )
 	
 
-	
-	
-
-	//console.log( streamToString(res) ) 
+	//console.log( streamToString(lodash.flatten(res)) ) 
 
 	/*let stack = [] 
 	let start = () => '{' 
